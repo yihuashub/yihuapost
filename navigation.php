bc50beb7cb87a447d98fc38661e46dffe74fd2c5
Yihua Huang <hyhhyhhyh@gmail.com>
2018-06-12 14:47:14 -0500

    bug fix, HTTPS supported

--- navigation.php
+++ navigation.php
@@ -10,54 +10,64 @@ include_once ("./system_files/classes/YihuaPost.php");
 include_once ("./system_files/classes/YihuaPostProfile.php");
 
 $checkRewrite = false;
+$siteLanguage = '';
 $rootUrl = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]/";
-$hostUrl = "//".$_SERVER[HTTP_HOST]."/";
+//$rootUrl = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]/";
+
+$hostUrl = "//".$_SERVER['HTTP_HOST']."/";
 
 $url=strtok($_SERVER["REQUEST_URI"],'?');
 $actual_link = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$url";
 $yihuapost = new YihuaPost();
 
-if ($_SERVER["REQUEST_METHOD"] == "GET") {
-    if (isset($_GET['url'])) {
-        $request = $_GET['url'];
+if($yihuapost->initialization()){
+    if ($_SERVER["REQUEST_METHOD"] == "GET" || $_SERVER["REQUEST_METHOD"] == "POST") {
+        if (isset($_GET['url']) || isset($_POST['url'])) {
 
-        if(empty($request))
-        {
-            $checkRewrite = true;
-            require("pages/home.php");
-        }
-        else{
-            $urlArray = (explode("/",$request));
-
-            if(count($urlArray) > 0)
+            if(isset($_GET['url']))
             {
-                $model = $yihuapost->getRewriteUrlModel($urlArray[0]);
+                $request = $_GET['url'];
+            }
+            else{
+                $request = $_POST['url'];
+            }
 
-                if(count($urlArray) == $yihuapost->getRewriteSize())
+            include_once ("./system_files/language.php");
+            $siteProfile = new YihuaPostProfile($siteLanguage,$yihuapost->getCurrentDB());
+
+            if(empty($request))
+            {
+                $checkRewrite = true;
+                require("pages/home.php");
+            }
+            else{
+                $urlArray = (explode("/",$request));
+
+                if(count($urlArray) > 0)
                 {
-                    if($model)
+                    $model = $yihuapost->getRewriteUrlModel($urlArray[0]);
+
+                    if(count($urlArray) <= $yihuapost->getRewriteSize())
                     {
-                        $checkRewrite = true;
-                        include_once ("./system_files/language.php");
-                        $siteProfile = new YihuaPostProfile($siteLanguage,$yihuapost->getCurrentDB());
-
-                        require("pages/".$model.".php");
-
+                        if($model)
+                        {
+                            $checkRewrite = true;
+                            require("pages/".$model.".php");
+                        }
                     }
                 }
             }
+            if(!$checkRewrite)
+            {
+                require ("pages/404.php");
+            }
+
+
+        } else {
+            require("pages/home.php");
         }
-
-
-        if(!$checkRewrite)
-        {
-            require ("pages/404.php");
-        }
-
-
-    } else {
-        require("pages/home.php");
     }
+}else{
+    require("config/install.php");
 }
 
-?>
